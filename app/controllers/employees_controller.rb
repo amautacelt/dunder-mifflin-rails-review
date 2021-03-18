ea37631f61b3ab6314294960c9902d3bb1165924
Carlos E. La Torre <amautacelt@gmail.com>
2021-03-18 15:30:56 -0400

    initial

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,12 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+        # render :index
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+        #render :show
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :dogs
-  resources :employees
+  # resources :dogs
+  # resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/dogs', to: 'dogs#index', as: 'dogs'
+  get '/employees', to: 'employees#index', as: 'employees'
+  get '/dogs/:id', to: 'dogs#show', as: 'dog'
+  get '/employees/:id', to: 'employees#show', as: 'employee'
+
 end
